969362ff90a916c8600081b80400234193b7566f
martin <martin.aubertin@90tech.fr>
2023-05-19 15:10:40 +0200

    formik install ok

--- src/components/Stepper/StepperComponent.tsx
+++ src/components/Stepper/StepperComponent.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useState} from 'react';
 import { JSX } from "react/jsx-runtime";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 export function StepperComponent(): JSX.Element {
-  const count: Number = 0;
-  console.log(React);
+  
+  const [submit, setSubmit] = useState(false);
+  const [count, setCount] = useState(1);
+  const maxCount: any = 10;
+
   return (
-  <>test {count}</>
+  <>
+    <Formik initialValues={{ name: '', surname: '', society: ''}}
+    validate={values => {
+      const errors: any = {};
+      if(!values.name) {
+        errors.email = 'Required';
+      } 
+      return errors;
+    }}
+    onSubmit={(values) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        setSubmit(true);
+      }, 400)
+    }}
+    >
+    <Container sx={{margin: "1rem"}}>
+      <Typography align='center' mt={5}> Etape <span>{count}/{maxCount}</span></Typography>
+      <Form style={{display: 'flex', flexDirection: "column", gap: "10px"}}>
+          <TextField label="name" type="text" name="name"></TextField>
+        <ErrorMessage name="name" component="div" />
+        <TextField type="text" label="surname" name="surname"></TextField>
+        <TextField type="text" label="society" name="society"></TextField>
+        <Button type="submit" onSubmit={() => setCount(2)}> Envoyer </Button>
+        
+      </Form>
+    </Container>
+
+    </Formik>
+  </>
   );
 }
