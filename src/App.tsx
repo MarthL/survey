ebe59d320e66328c620201ca12350e4a0c46af15
martin <martin.aubertin@90tech.fr>
2023-05-22 00:17:19 +0200

    adding particle lib

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ import { Header } from './components/Temp/Header/Header';
 import { Container } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { LegalMentions } from './components/Temp/LegalMentions/LegalMentions';
+import Particle from './components/Particle.jsx';
 
 function App() {
   const [count, setCount] = useState(1);
+  
   return (
     <>
       <Router>
         <Header />
         <Container maxWidth="sm">
+        <Particle />
         <Routes>
         <Route path="/" element={ <StepperComponent count={count} />}></Route>
         <Route path="/legal" element={<LegalMentions />}></Route>
