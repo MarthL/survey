94e61279258c675269ad9b187c66476a0a30c124
martin <martin.aubertin@90tech.fr>
2023-05-22 23:13:21 +0200

    add transition

--- src/components/Stepper/Step1.tsx
+++ src/components/Stepper/Step1.tsx
@@ -7,77 +7,85 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import styles from './StepperComponent.module.css';
 import { Box } from '@mui/material';
-import {RingLoader} from 'react-spinners';
+import { RingLoader } from 'react-spinners';
 
 export function Step1(props: any): JSX.Element {
 
-const [submit, setSubmit] = useState(false);
-const [isLoading, setIsLoading] = useState(true);
+  const [submit, setSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-const { count, setCount } = props;
+  const { count, setCount } = props;
 
-const errors = {};
+  const errors = {};
 
-useEffect(() => {
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 5000);
-}, []);
-
-const validate = (values: any) => {
-  const errors: any = {};
-  if (!values.name || values.name.length < 1) {
-    errors.name = 'This field is required';
-  }
-  if (!values.surname || values.surname.length < 1) {
-    errors.surname = 'This field is required';
-  }
-  if (!values.society || values.society.length < 1) {
-    errors.society = 'This field is required';
-  }
-  return errors;
-};
-
-return (
-  <>
-    <div>
-    {isLoading ? (
-      <><Box textAlign='center'> Loading please wait </Box><div className="loader-container" style={{ textAlign: 'center' }}>
-          <RingLoader color="#000" size={500} />
-        </div>
-      </>
-    ) : (
-      <>
-      <Typography align='center' fontStyle="oblique">Please give us some details about you</Typography>
-      <Formik initialValues={{ name: '', surname: '', society: '' }}
-        validate={validate}
-        onSubmit={(values) => {
-          setTimeout(() => {
-            // alert(JSON.stringify(values, null, 2));
-            setSubmit(true);
-            setCount(2);
-            console.log(count);
-          }, 400)
-        }}
-      >
-        <Container sx={{ margin: "1rem" }}>
-          <Form style={{ display: 'flex', width: '100%', height:'70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
-            <Field as={TextField} label="name" type="text" name="name" >  </Field>
-            <div className={styles.Error}> <ErrorMessage name="name"/> </div>
-            <Field as={TextField} type="text" label="surname" name="surname"></Field>
-            <div className={styles.Error}> <ErrorMessage name="surname"/> </div>
-            <Field as={TextField} type="text" label="society" name="society"></Field>
-            <div className={styles.Error}> <ErrorMessage name="society"/> </div>
-            <Box textAlign="center">
-            <Button variant="contained" type="submit" onSubmit={() => setCount(2)}> Send </Button>
-            </Box>
-          </Form>
-        </Container>
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 6000);
+  }, []);
   
-      </Formik>
+
+  const validate = (values: any) => {
+    const errors: any = {};
+    if (!values.name || values.name.length < 1) {
+      errors.name = 'This field is required';
+    }
+    if (!values.surname || values.surname.length < 1) {
+      errors.surname = 'This field is required';
+    }
+    if (!values.society || values.society.length < 1) {
+      errors.society = 'This field is required';
+    }
+    return errors;
+  };
+
+  return (
+    <>
+      <div>
+        {isLoading ? (
+          <>
+            <div className="loader-container" style={{ textAlign: 'center' }}>
+            <RingLoader color="#000" size={500} />
+          </div>
+          </>
+        ) : (
+          <>
+            <div className={`${styles.container} ${styles['fade-in']}`}>
+              <Typography align='center' fontStyle="oblique">Please give us some details about you</Typography>
+            <Formik initialValues={{ name: '', surname: '', society: '' }}
+              validate={validate}
+              onSubmit={(values) => {
+                setTimeout(() => {
+                  // alert(JSON.stringify(values, null, 2));
+                  setSubmit(true);
+                  setCount(2);
+                  console.log(count);
+                }, 400)
+              }}
+            >
+              <Container sx={{ margin: "1rem" }}>
+                <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
+                  <Field as={TextField} label="name" type="text" name="name" >  </Field>
+                  <div className={styles.Error}> <ErrorMessage name="name" /> </div>
+                  <Field as={TextField} type="text" label="surname" name="surname"></Field>
+                  <div className={styles.Error}> <ErrorMessage name="surname" /> </div>
+                  <Field as={TextField} type="text" label="society" name="society"></Field>
+                  <div className={styles.Error}> <ErrorMessage name="society" /> </div>
+                  <Box textAlign="center">
+                    <Button variant="contained" type="submit" onSubmit={() => setCount(2)}> Send </Button>
+                  </Box>
+                </Form>
+              </Container>
+
+            </Formik>
+            </div>
+          </>
+        )}
+      </div>
     </>
-    )}
-    </div>
-  </>
-);
+  );
 }
