29bbe20460039bd25bd814e4e7285f37bcdfb241
martin <martin.aubertin@90tech.fr>
2023-05-25 15:02:43 +0200

    clean code

--- src/components/Stepper/Step4/FeaturesForm.tsx
+++ src/components/Stepper/Step4/FeaturesForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { JSX } from "react/jsx-runtime";
 import { TextField, Button, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import { Radio, RadioGroup, List, ListItem, ListItemDecorator } from '@mui/joy';
@@ -6,62 +6,54 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export function FeaturesForm(props: any): JSX.Element {
   const { count, setCount } = props;
-  const [item, setItem] = useState('');
-  const [selectedValue, setSelectedValue] = useState('');
 
-  const technos = ['PHP', 'Javascript', 'React', 'Angular', 'Symfony', 'NodeJS', 
-    'Express', 'MySQL', 'C#', 'NestJS', 'Laravel', 'HTML/CSS', 
+  const technos = [
+    'PHP', 'Javascript', 'React', 'Angular', 'Symfony', 'NodeJS',
+    'Express', 'MySQL', 'C#', 'NestJS', 'Laravel', 'HTML/CSS',
     'Design', 'SEO', 'Others'
   ];
-
-  const handleChange = (event: SelectChangeEvent<string>) => {
-    setSelectedValue(event.target.value);
-  };
-
-  
 
   return (
     <>
       <Formik
-        initialValues={{ objective: '', environment: '', language: item }}
+        initialValues={{ objective: '', environment: '', language: '' }}
         onSubmit={(values: any) => {
-          setItem(values.language);
           console.log(values);
         }}
       >
-        {({ values, setFieldValue }) => (
-        <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
-          <Field as={TextField} label="objective" placeholder="Which solution this enhancements is about ?" name="objective"> </Field>
-          <Field as={TextField} label="environment" placeholder="Please provides any informations regarding environment/specificities" name="environment"> </Field>
-          <Select 
-            placeholder="Which language will be solicited ?" 
-            name="language"
-            value={selectedValue}
-            MenuProps={{
-              PaperProps: {
-                style: {
-                  maxHeight: '250px'
-                }
-              },
-              disableScrollLock: true,
-            }}
-            onChange={(event: any) => {
-              setFieldValue("language", event.target.value);
-              handleChange(event);
-            }}
-          >
-            {technos.sort().map((item) => (
-              <MenuItem aria-label="Which language is concerned with that feature" value={item}>{item}</MenuItem>
-            ))}
-        </Select>
+        {({ values, handleChange, handleSubmit }) => (
+          <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
+            <Field as={TextField} label="objective" placeholder="Which solution this enhancements is about ?" name="objective" />
 
-          <Button variant="contained" type="submit" onSubmit={() => setCount(5)}> Send </Button>
-        </Form>
+            <Field as={TextField} label="environment" placeholder="Please provides any informations regarding environment/specificities" name="environment" />
+
+            <Select
+              placeholder="Which language will be solicited ?"
+              name="language"
+              value={values.language}
+              MenuProps={{
+                PaperProps: {
+                  style: {
+                    maxHeight: '250px'
+                  }
+                },
+                disableScrollLock: true,
+              }}
+              onChange={handleChange}
+            >
+              {technos.sort().map((item) => (
+                <MenuItem key={item} aria-label="Which language is concerned with that feature" value={item}>{item}</MenuItem>
+              ))}
+            </Select>
+
+            <Button variant="contained" type="submit" onClick={() => console.log(' ')}> Send </Button>
+          </Form>
         )}
       </Formik>
     </>
   )
 }
+
 
 // Qui sont les utilisateurs cibles de cette fonctionnalité ? Ont-ils des caractéristiques spécifiques à prendre en compte ?
 
