d231e7f9607e305fda3665d128d07a3cf52945e3
martin <martin.aubertin@90tech.fr>
2023-06-01 23:25:23 +0200

    add error handler

--- src/components/Stepper/Step4/FeaturesForm.tsx
+++ src/components/Stepper/Step4/FeaturesForm.tsx
@@ -23,37 +23,37 @@ export function FeaturesForm(props: any): JSX.Element {
       >
         {({ values, handleChange, handleSubmit }) => (
           <Container sx={{ margin: "1rem" }}>
-          <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
-            <Field as={TextField} label="objective" placeholder="Which solution this enhancements is about ?" name="objective" />
+            <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
+              <Field as={TextField} label="objective" placeholder="Which solution this enhancements is about ?" name="objective" />
 
-            <Field as={TextField} label="environment" placeholder="Please provides any informations regarding environment/specificities" name="environment" />
+              <Field as={TextField} label="environment" placeholder="Please provides any informations regarding environment/specificities" name="environment" />
 
-            <Select
-              placeholder="Which language will be solicited ?"
-              name="language"
-              displayEmpty
-              renderValue={(selected) => (selected ? selected : "Which language will be solicited ?")}
-              value={values.language}
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: '250px'
-                  }
-                },
-                disableScrollLock: true,
-              }}
-              onChange={handleChange}
-            >
-              {(<MenuItem value="" disabled> Which language is concerned with that feature </MenuItem>)}
-              {technos.sort().map((item) => (
-                <MenuItem key={item} aria-label="Which language is concerned with that feature" value={item}>{item}</MenuItem>
-              ))}
-            </Select>
-            <Box textAlign="center" display="flex" alignItems='center' justifyContent='space-evenly'>
-            <Button variant="contained" color="error" onClick={() => setCount(3)}> Back </Button>
-              <Button variant="contained" type="submit" onClick={() => setCount(5)}> Send </Button>
-            </Box> 
-          </Form>
+              <Select
+                placeholder="Which language will be solicited ?"
+                name="language"
+                displayEmpty
+                renderValue={(selected) => (selected ? selected : "Which language will be solicited ?")}
+                value={values.language}
+                MenuProps={{
+                  PaperProps: {
+                    style: {
+                      maxHeight: '250px'
+                    }
+                  },
+                  disableScrollLock: true,
+                }}
+                onChange={handleChange}
+              >
+                {(<MenuItem value="" disabled> Which language is concerned with that feature </MenuItem>)}
+                {technos.sort().map((item) => (
+                  <MenuItem key={item} aria-label="Which language is concerned with that feature" value={item}>{item}</MenuItem>
+                ))}
+              </Select>
+              <Box textAlign="center" display="flex" alignItems='center' justifyContent='space-evenly'>
+                <Button variant="contained" color="error" onClick={() => setCount(3)}> Back </Button>
+                <Button variant="contained" type="submit" onClick={() => setCount(5)}> Send </Button>
+              </Box>
+            </Form>
           </Container>
         )}
       </Formik>
--- src/components/Stepper/Step4/OtherForm.tsx
+++ src/components/Stepper/Step4/OtherForm.tsx
@@ -3,20 +3,35 @@ import { JSX } from "react/jsx-runtime";
 import { TextField, Button, Select, MenuItem, SelectChangeEvent, Container, Box } from "@mui/material";
 import { Radio, RadioGroup, List, ListItem, ListItemDecorator } from '@mui/joy';
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import styles from './StepperComponent.module.css';
 
 export function OtherForm(props: any): JSX.Element {
 
+  interface FormErrors {
+    reason?: string;
+  }
+
   const { setCount } = props;
+
+  const validate = (values: any): FormErrors => {
+    const errors: FormErrors = {};
+    if (!values.reason || values.reason.length < 1) {
+      errors.reason = 'This field is required';
+    }
+    return errors;
+  };
 
   return (
     <>
       <Formik
         initialValues={{ reason: '' }}
         onSubmit={(values: any) => console.log(values)}
+        validate={validate}
       >
         <Container sx={{ margin: "1rem" }}>
           <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
             <Field as={TextField} label="reason" name="reason"> </Field>
+            <div className={styles.Error}> <ErrorMessage name="reason" /> </div>
             <Box textAlign="center" display="flex" alignItems='center' justifyContent='space-evenly'>
               <Button variant="contained" color="error" onClick={() => setCount(3)}> Back </Button>
               <Button variant="contained" type="submit"
