19ec8847d612aaf47280ba0696e5cb372af0714f
martin <martin.aubertin@90tech.fr>
2023-06-01 23:18:51 +0200

    added else

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Step1 } from './components/Stepper/Step1';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+// import { Step1 } from './components/Stepper/Step1';
 
-// test('renders step1', () => {
-//   render(<Step1 />);
-//   const test = screen.getByText("Send");
-//   expect(test).toBeInTheDocument();
-// });
+// // test('renders step1', () => {
+// //   render(<Step1 />);
+// //   const test = screen.getByText("Send");
+// //   expect(test).toBeInTheDocument();
+// // });
--- src/components/Stepper/Step4/OtherForm.tsx
+++ src/components/Stepper/Step4/OtherForm.tsx
@@ -5,5 +5,26 @@ import { Radio, RadioGroup, List, ListItem, ListItemDecorator } from '@mui/joy';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export function OtherForm(props: any): JSX.Element {
-  return (<>test</>)
+
+  const { setCount } = props;
+
+  return (
+    <>
+      <Formik
+        initialValues={{ reason: '' }}
+        onSubmit={(values: any) => console.log(values)}
+      >
+        <Container sx={{ margin: "1rem" }}>
+          <Form style={{ display: 'flex', width: '100%', height: '70vh', flexDirection: "column", gap: "10px", justifyContent: 'center' }}>
+            <Field as={TextField} label="reason" name="reason"> </Field>
+            <Box textAlign="center" display="flex" alignItems='center' justifyContent='space-evenly'>
+              <Button variant="contained" color="error" onClick={() => setCount(3)}> Back </Button>
+              <Button variant="contained" type="submit"
+              > Next </Button>
+            </Box>
+          </Form>
+        </Container>
+      </Formik>
+    </>
+  )
 }
